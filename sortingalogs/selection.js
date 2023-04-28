a662c3179872adc307d1cad15f64320e42647ce1
Anshul kusumbe <81466016+anshul26k@users.noreply.github.com>
2023-04-28 23:20:23 +0530

    Add files via upload

---
+++ sortingalogs/selection.js
@@ -0,0 +1,38 @@
+async function Selection(){
+        let ele = document.querySelectorAll(".bar");
+        for(let i=0;i<ele.length;i++){
+            let temp = i;
+            ele[i].style.background = "blue";
+
+            for(let j=i+1;j<ele.length;j++){
+                    ele[j].style.background = "red";
+
+                    await waitforme(delay);
+                    if(parseInt(ele[j].style.height)<parseInt(ele[temp].style.height)){
+                        ele[temp].style.background = "cyan";
+                        temp = j;
+                    }else{
+                        ele[j].style.background = "cyan";
+                    }
+                   
+            }
+            await waitforme(delay);
+            swap(ele[i],ele[temp]);
+            ele[temp].style.background = "cyan";
+            ele[i].style.background = "green";
+        }
+}
+
+
+let selection_sort_method = document.querySelector(".selectionSort");
+selection_sort_method.addEventListener("click",async function(){
+    selection_sort_method.classList.add("selected-btn");
+    disable_size_slider();
+    disable_sorting();
+    disable_new_array();
+    await Selection();
+    selection_sort_method.classList.remove("selected-btn");
+    enable_size_slider();
+    enable_sorting();
+    enable_new_array();
+});
